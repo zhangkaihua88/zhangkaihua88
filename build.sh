8ba40d2a95a5b8ad2e9533e237f47e3683c07e1c
zhangkaihua88 <zhangkaihua@buaa.edu.cn>
2023-10-07 09:49:17 +0800

    dix

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
 python addTable.py
 python addActivity.py
+
+git config --local user.name actions-user
+git config --local user.email "actions@github.com" 
+git fetch
+git add *
+git commit -am "GH Action File removed $(date)" 
+git push -f origin main
